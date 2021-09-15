d2ebd2ebd0c24b2e29bb39dedd2a30039d9b35a5
Andrey Vinokurov <kreketjot@gmail.com>
2021-09-15 09:34:39 +0300

    generator added

--- src/js/generators.js
+++ src/js/generators.js
@@ -7,9 +7,20 @@
  * @returns Character type children (ex. Magician, Bowman, etc)
  */
 export function* characterGenerator(allowedTypes, maxLevel) {
-  // TODO: write logic here
+  const types = [...allowedTypes];
+  const rand = (max) => Math.floor(Math.random() * max);
+  while (true) {
+    const randType = rand(types.length);
+    const randLevel = rand(types.maxLevel) + 1;
+    yield new types[randType](randLevel);
+  }
 }
 
 export function generateTeam(allowedTypes, maxLevel, characterCount) {
-  // TODO: write logic here
+  const types = [];
+  for (let i = 0; i < characterCount; i++) {
+    const type = characterGenerator(allowedTypes, maxLevel).next();
+    types.push(type);
+  }
+  return types;
 }
