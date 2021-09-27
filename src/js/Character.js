981467b4ed0c84984f0f1ad14c6ca93061de8aea
Andrey Vinokurov <kreketjot@gmail.com>
2021-09-27 19:44:56 +0300

    set/get for Character & edit lint rule about _

--- src/js/Character.js
+++ src/js/Character.js
@@ -3,12 +3,26 @@ import './roundNumber';
 export default class Character {
   constructor(level, type = 'generic', maxHealth = 100) {
     this.level = level;
-    this.health = 50;
+    this._health = 50;
     this.maxHealth = maxHealth;
     this.type = type;
     if (new.target === Character) {
       throw new Error('class Character can\'t be instantiated');
     }
+  }
+
+  set health(hp) {
+    if (hp > this.maxHealth) {
+      this._health = this.maxHealth;
+    } else if (hp < 0) {
+      this._health = 0;
+    } else {
+      this._health = hp;
+    }
+  }
+
+  get health() {
+    return this._health;
   }
 
   levelUp() {
